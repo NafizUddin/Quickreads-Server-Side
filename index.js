1fc305f6dd455cdce6f5fa5dfb3bfb96eb6e2dac
NafizUddin <nafizuddin.ctg@gmail.com>
2023-11-05 15:59:28 +0600

    added category and JWT related API

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+
+// Default Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"], // Client Side Server
+    credentials: true,
+  })
+);
+
+// Manual Middlewares
+const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.cookies?.token;
+    if (token) {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+          return res
+            .status(401)
+            .send({ status: 401, message: "Unauthorized Access" });
+        } else {
+          req.user = decoded;
+          next();
+        }
+      });
+    } else {
+      return res
+        .status(401)
+        .send({ status: 401, message: "Unauthorized Access" });
+    }
+  } catch (error) {
+    res.status(401).send("Unauthorized");
+  }
+};
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster0.wixlrgj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    const categoryCollection = client
+      .db("quickReadsDB")
+      .collection("categories");
+
+    // JWT Related API
+
+    app.post("/api/auth/access-token", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      const expirationDate = new Date();
+      expirationDate.setDate(expirationDate.getDate() + 7);
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          expires: expirationDate,
+        })
+        .send({ success: true });
+    });
+
+    app.post("/api/auth/logout", async (req, res) => {
+      const user = req.body;
+      console.log("user in the token", user);
+      res
+        .clearCookie("token", { maxAge: 0, sameSite: "none", secure: true })
+        .send({ success: true });
+    });
+
+    // Category Related API
+
+    app.get("/api/categories", async (req, res) => {
+      const result = await categoryCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/api/categories", async (req, res) => {
+      const newCategory = req.body;
+      const result = await categoryCollection.insertOne(newCategory);
+      res.send(result);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running perfectly");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
